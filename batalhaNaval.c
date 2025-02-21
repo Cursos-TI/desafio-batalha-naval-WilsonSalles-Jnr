fb1b20a75e4012b71f20ffb450c11b38708973a3
Wilson Salles Junior <wilsonsallesjunior@gmail.com>
2025-02-21 20:30:27 -0300

    Nível novato: posicionando navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,11 +4,72 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+void validaLinha(int LIMITE, int input)
+{
+}
+
+int main()
+{
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
+    // Valor máximo de linha e coluna
+    int LINHA = 5, COLUNA = 5;
+
+    int tabuleiro[LINHA][COLUNA];
+    int NAVIOS_MAX = 3;
+
+    // Definindo todas as casas como 0;
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            tabuleiro[i][j] = 0;
+        }
+    }
+    do
+    {
+        int linha, coluna;
+        printf("Posicione os navios (%i Restantes):\n", NAVIOS_MAX);
+
+        do
+        {
+            printf("Linha: ");
+            scanf("%i", &linha);
+            while (linha > LINHA || linha <= 0)
+            {
+                printf("O limite é de %d linhas, insira outro valor: ", LINHA);
+                scanf("%i", &linha);
+            };
+
+            printf("Coluna: ");
+            scanf("%i", &coluna);
+            while (coluna > COLUNA || coluna <= 0)
+            {
+                printf("O limite é de %d linhas, insira outro valor: ", COLUNA);
+                scanf("%i", &coluna);
+            };
+            if (tabuleiro[linha - 1][coluna - 1])
+            {
+                printf("Navio já posicionado, escolha outra posição!\n");
+            }
+
+        } while (tabuleiro[linha - 1][coluna - 1]);
+        tabuleiro[linha - 1][coluna - 1] = 1;
+        NAVIOS_MAX--;
+    } while (NAVIOS_MAX);
+
+    printf("Seu tabuleiro: \n");
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            printf("%i ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -25,7 +86,7 @@ int main() {
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 1 1 1 1 1
-    
+
     // Exemplo para habilidade em octaedro:
     // 0 0 1 0 0
     // 0 1 1 1 0
