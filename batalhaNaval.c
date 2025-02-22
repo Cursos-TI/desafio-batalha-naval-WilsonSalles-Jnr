d00762d29bcc2c94bfb59321af905914c9e15016
Wilson Salles Junior <wilsonsallesjunior@gmail.com>
2025-02-22 01:39:11 -0300

    Nível Mestre: selecionando habilidade

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -18,8 +18,16 @@ int main()
     // Valor máximo de linha e coluna
     int LINHA = 10, COLUNA = 10;
 
+    int pivot = 3;
+
     int tabuleiro[LINHA][COLUNA];
-    int NAVIOS_MAX = 4;
+
+    // Próxima sequência é de 3, inicio é igual a (5 - área atingida) / 2
+    int habilidadeCone[2] = {3, 5};
+    int habilidadeOctaedro[2] = {3, 0};
+    int habilidadeCruz[2] = {5, 0};
+
+    int NAVIOS_MAX = 1;
 
     // Definindo todas as casas como 0;
     for (int i = 0; i < LINHA; i++)
@@ -29,6 +37,7 @@ int main()
             tabuleiro[i][j] = 0;
         }
     }
+
     do
     {
         int linha, coluna;
@@ -60,6 +69,72 @@ int main()
         tabuleiro[linha - 1][coluna - 1] = 3;
         NAVIOS_MAX--;
     } while (NAVIOS_MAX);
+
+    printf("Seu tabuleiro: \n");
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            printf("%i ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    int linhaAtaque, colunaAtaque;
+    printf("Escolha uma área para disparar atacar: \n");
+    printf("Linha: ");
+    scanf("%i", &linhaAtaque);
+    printf("Coluna: ");
+    scanf("%i", &colunaAtaque);
+
+    linhaAtaque--;
+    colunaAtaque--;
+    tabuleiro[linhaAtaque][colunaAtaque] = 1;
+
+    int opcao;
+    int *habilidade;
+
+    printf("Seleciona a habilidade: \n");
+    printf("1 - Cone [padrão] \n");
+    printf("2 - Octaedro \n");
+    printf("3 - Cruz \n");
+    scanf("%i", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        habilidade = habilidadeCone;
+        break;
+    case 2:
+        habilidade = habilidadeOctaedro;
+        break;
+    case 3:
+        habilidade = habilidadeCruz;
+        break;
+
+    default:
+        habilidade = habilidadeCone;
+        break;
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        int jLoopCount = 0;
+        tabuleiro[linhaAtaque + 1 + i][colunaAtaque] = 1;
+        for (int j = 0; j < habilidade[i] - jLoopCount; j++)
+        {
+            tabuleiro[linhaAtaque + 1 + i][colunaAtaque] = 1;
+            if (colunaAtaque - (1 + j) >= 0)
+            {
+                tabuleiro[linhaAtaque + 1 + i][colunaAtaque - (1 + j)] = 1;
+            }
+            if (colunaAtaque + (1 + j) < COLUNA)
+            {
+                tabuleiro[linhaAtaque + 1 + i][colunaAtaque + (1 + j)] = 1;
+            }
+            jLoopCount += 2;
+        }
+    }
 
     printf("Seu tabuleiro: \n");
     for (int i = 0; i < LINHA; i++)
